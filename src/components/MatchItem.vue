0ddedd214499b1030358e74fc90eab0220179c34
arvind <arvindputtu@gmail.com>
2020-09-29 13:56:14 +0530

    first commit

---
+++ src/components/MatchItem.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="match-item">
+        <p> Match No. {{match.id}}</p>
+        <p> Location: {{match.city}}</p>
+        <p>Played on {{match.date}}</p>
+        <p> Venue : {{match.venue}}</p>
+        <p> {{match.team1}} VS {{match.team2}}</p>
+          
+        <p> Toss won by {{match.toss_winner}} and chose to {{match.toss_decision}} first</p>
+        <p> Match result: {{match.result}}</p>
+        <p> Match was won by {{match.winner}} by {{match.win_by_runs}} runs and by {{match.win_by_wickets}} wickets</p>
+        <p>Man of the match:{{match.player_of_match}}</p> 
+        <p>Umpires on the field were : {{match.umpire1}}
+            ,{{match.umpire2}}
+           
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"MatchItem",
+    props:["match"],
+    method:{
+        getTeamImage: function(teamName) {
+            if(teamName === 'Sunrisers Hyderabad')
+            return (
+            <img alt="SRH LOGO" src="./assets/teams/SRH.png"/>
+            );
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+ .match-item{
+background-color: #20bf55;
+background-image: linear-gradient(315deg, #20bf55 0%, #01baef 74%);
+
+
+ }
+ 
+
+</style>
---
+++ src/components/Matches.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="match">
+       <div  v-bind:key="match.id" v-for="match in matches">
+         <MatchItem v-bind:match="match"/>
+
+       </div>
+    </div>
+</template>
+
+<script>
+import MatchItem from './MatchItem.vue';
+export default {
+    name:"IPL Matches stats",
+    components:{
+        MatchItem
+    }
+    ,
+    props:["matches"]
+}
+</script>
+
+<style scoped>
+ .match {
+  background-color: #eec0c6;
+background-image: linear-gradient(315deg, #eec0c6 0%, #7ee8fa 74%);
+;
+
+ }
+</style>
